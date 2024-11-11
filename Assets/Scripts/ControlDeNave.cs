eb6c7a49471e8e3aac58981459f21ab4117a4983
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2024-11-11 12:01:26 -0500

    Se guardo los cambios desde el lab5

--- Assets/Scripts/ControlDeNave.cs
+++ Assets/Scripts/ControlDeNave.cs
@@ -5,31 +5,64 @@ using UnityEngine;
 public class ControlDeNave : MonoBehaviour
 {
     Rigidbody rigidbody;
+    Transform transform;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //print("Hola");
+        //Debug.Log(Time.deltaTime + "seg. " + (1.0 / Time.deltaTime));
         ProcesarInput();
     }
     private void ProcesarInput()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidbody.AddRelativeForce(Vector3.up);
+        Propulsion();
+        Rotacion();
+    }
 
-        }
+    private void Rotacion()
+    {
         if (Input.GetKey(KeyCode.D))
         {
-            print("Rot derecha");
+            //transform.Rotate(-Vector3.forward);
+            var rotarDerecha = transform.rotation;
+            rotarDerecha.z -= Time.deltaTime * 1;
+            transform.rotation = rotarDerecha;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            print("Rot izquierda");
+            //transform.Rotate(Vector3.forward);
+            var rotarIzquierda = transform.rotation;
+            rotarIzquierda.z += Time.deltaTime * 1;
+            transform.rotation = rotarIzquierda;
         }
+    }
+
+    private void Propulsion()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //print("espacio a");
+            rigidbody.freezeRotation = true;
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        rigidbody.freezeRotation = false;
     }
 }
